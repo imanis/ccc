1e7ff5d4b0ba49fa2ea9fa68181c4d198edb1905
Youssef EL AKARI <y.elakari@maptek.ma>
2016-09-25 01:07:42 +0200

    adding twitter reply

--- tweet-crawler/src/main/java/carrefour/ccc/twitter/TweetCrawler.java
+++ tweet-crawler/src/main/java/carrefour/ccc/twitter/TweetCrawler.java
@@ -1,5 +1,16 @@
-package carrefour.ccc.twitter;
+package carrefour.ccc.twitter ;
 
+import java.awt.List;
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.UUID;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function;
@@ -7,43 +18,124 @@ import org.apache.spark.streaming.Duration;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
-import org.apache.spark.streaming.dstream.DStream;
 import org.apache.spark.streaming.twitter.TwitterUtils;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.*;
+
 import twitter4j.Status;
+import twitter4j.StatusUpdate;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 import twitter4j.auth.Authorization;
 import twitter4j.auth.AuthorizationFactory;
 import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationContext;
 
-import java.util.Arrays;
-
 /**
  * Created by mehdi on 24/09/16.
  */
-public class TweetCrawler {
+public class TweetCrawler implements Serializable {
 
     static final String PATH = "/tmp/tweets";
 
+   static  final TransportClient client = new  TransportClient.Builder().build();
+	       
+    @SuppressWarnings("deprecation")
+	public static void main(String[] args) throws UnknownHostException {
 
-    public static void main(String[] args) {
-
-
+    	Logger.getLogger("org").setLevel(Level.OFF);
+    	Logger.getLogger("akka").setLevel(Level.OFF);
+    	
         SparkConf sparkConf = new SparkConf().setAppName("Tweet Crawler").setMaster("local[2]");
+ 
         JavaStreamingContext sc = new JavaStreamingContext(sparkConf, new Duration(5000));
 
-        //String[] filters = {"fraise", "mandarine", "cerise", "legume", "fruit", "raisin"};
-        String[] filters = {};
+        String[] filters = {"fraise", "mandarine", "cerise", "legume", "fruit", "raisin"};
         JavaReceiverInputDStream<Status> stream = selectTweetsByFilter(sc, filters);
+        
+        client.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("91.134.229.137"),9300
+        		));
+		
+        JavaDStream<Status> jdsteam  = selectTweetsByLang(stream,"fr");
+        jdsteam.
+        foreachRDD( new Function<JavaRDD<Status>, Void>() {
+        	
+			private static final long serialVersionUID = 1L;
 
+			public Void call(JavaRDD<Status> rdd) throws Exception {
+				
+				
+				rdd.foreach(tweet -> {
+				
+					
+					int i=0;
+					String term="";
+					while(i<filters.length && !tweet.getText().toLowerCase().contains(filters[i].toLowerCase()))
+					{
+						i++;
+					}
+					if(i<filters.length)
+					{
+						term = filters[i];
+					}
+					 
+					XContentBuilder 
+					builder
+					 = jsonBuilder()
+						    .startObject()
+						    .field("user", tweet.getUser().getScreenName())
+					        .field("post_date",tweet.getCreatedAt())
+					        .field("message", tweet.getText())
+					        .field("STO_EAN","3020180189392")
+					        .field("STORE_DESC", "CITY PARIS -HOPITAL 107")
+					       .field("term", term)
+						    .endObject();
+					
+					IndexResponse response = client.prepareIndex("twitter_1", "tweet")
+					        .setSource(builder)           
+					        .get();
+					
+					if(tweet.getUser().getScreenName().contains("im_anis")||tweet.getUser().getScreenName().toLowerCase().contains("p3rs0nne".toLowerCase()))
+						TweetReply(tweet,term);
+					
+				
+				});
+				return null;
 
-        //selectTweetsByLang(stream, "fr").print();
-        selectTweetsByLang(selectTweetsByLocation(stream, "paris"), "fr").print();
+}
 
+          });
 
         sc.start();
         sc.awaitTermination();
 
 
+    }
+    
+    static public void TweetReply(Status tweet ,String term)
+    {
+    	
+    	Twitter twittFact = new TwitterFactory().getInstance();
+		StatusUpdate st = new StatusUpdate("hello");
+		Status reply = null;
+		st.inReplyToStatusId(tweet.getId());
+		String message =UUID.randomUUID()+" Bonjour @" + tweet.getUser().getScreenName();
+	//	message +=  " en parlant de " + term;
+//		message +=  " vous trouverez nos promos sur";		
+	//	message +=  " http://91.134.229.135:8081/user="+UUID.randomUUID();
+		try {
+			  reply = twittFact.updateStatus(new StatusUpdate(message).inReplyToStatusId(tweet.getId()));
+			  System.out.println("Posted reply " + reply.getId() + " in response to tweet " + reply.getInReplyToStatusId());
+		  } catch (TwitterException e) {
+		    // TODO Auto-generated catch block
+		    e.printStackTrace();
+		  }
+	
     }
 
     static public JavaReceiverInputDStream<Status> selectTweetsByFilter(JavaStreamingContext sc, String[] filters) {
